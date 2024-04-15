46ee5a3ef2a2447fabcdaf08662d635aa19e2e56
Devpe007 <anddev007pe@gmail.com>
2024-04-15 15:48:22 -0300

    update: attaching an inline image to email template

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,13 @@
 class OrderMailer < ApplicationMailer
   def created(order)
+    attachments.inline['logo.png'] = logo
+
     @order = order
 
     mail to: @order.person.email, subject: "Pedido #{@order.id} recebido!"
+  end
+
+  def logo
+    File.read("#{Rails.root}/app/assets/images/logo.png")
   end
 end
